17ec75260a490e431eb49a49c3a16939e36884bb
Ondřej Kříž <xkrizo05@stud.fit.vutbr.cz>
2025-12-05 10:49:59 +0100

    first commit

---
+++ TaskApp/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using TaskApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// builder.Services.AddDbContext<>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
